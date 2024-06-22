0c478248bd8b4127a91502490609593f4c61d8cf
Ian Knight <ian.knight.1990@gmail.com>
2024-06-22 15:48:32 +0100

    Set up very basic summary screen

--- app/src/main/java/uk/nottsknight/basicdailybudget/MainActivity.kt
+++ app/src/main/java/uk/nottsknight/basicdailybudget/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.createGraph
+import uk.nottsknight.basicdailybudget.ui.SummaryScreen
 import uk.nottsknight.basicdailybudget.ui.theme.BasicDailyBudgetTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +25,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BasicDailyBudgetTheme {
+                val navController = rememberNavController()
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    BdbContent(navHostController = navController, innerPadding = innerPadding)
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+private const val PATH_SUMMARY_SCREEN = "summary"
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BasicDailyBudgetTheme {
-        Greeting("Android")
+fun BdbContent(navHostController: NavHostController, innerPadding: PaddingValues) {
+    Surface(modifier = Modifier.padding(innerPadding)) {
+        NavHost(
+            navController = navHostController,
+            graph = navHostController.createGraph(PATH_SUMMARY_SCREEN) {
+                composable(PATH_SUMMARY_SCREEN) { SummaryScreen() }
+            })
     }
 }
---
+++ app/src/main/java/uk/nottsknight/basicdailybudget/ui/SummaryScreen.kt
@@ -0,0 +1,71 @@
+package uk.nottsknight.basicdailybudget.ui
+
+import android.app.Application
+import android.icu.text.DateFormat
+import android.icu.text.DecimalFormat
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import androidx.room.Room
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import uk.nottsknight.basicdailybudget.R
+import uk.nottsknight.basicdailybudget.model.AccountRepository
+import uk.nottsknight.basicdailybudget.model.BdbDatabase
+import java.time.Instant
+
+@Composable
+fun SummaryScreen(viewModel: SummaryScreenViewModel = viewModel(factory = SummaryScreenViewModel.Factory)) {
+    val dailySpend = viewModel.dailySpend.collectAsState()
+    val spendFormatter = DecimalFormat.getCurrencyInstance()
+
+    val nextPayday = viewModel.nextPayday.collectAsState()
+    val paydayFormatter = DateFormat.getInstance()
+
+    Column(horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(stringResource(R.string.dailySpendLabel))
+        Text(spendFormatter.format(dailySpend.value / 100f))
+
+        Text(stringResource(R.string.nextPaydayLabel))
+        Text(paydayFormatter.format(nextPayday.value))
+    }
+}
+
+class SummaryScreenViewModel(private val accountRepo: AccountRepository) : ViewModel() {
+    private val dailySpendState = MutableStateFlow(0)
+    private val nextPaydayState = MutableStateFlow(Instant.now())
+
+    val dailySpend: StateFlow<Int> get() = dailySpendState
+    val nextPayday: StateFlow<Instant> get() = nextPaydayState
+
+    init {
+        getModelValues()
+    }
+
+    private fun getModelValues() = viewModelScope.launch {
+        val account = accountRepo.select(0) ?: return@launch
+        dailySpendState.value = account.dailyAllowance
+        nextPaydayState.value = account.nextPayday
+    }
+
+    companion object {
+        val Factory = viewModelFactory {
+            initializer {
+                val context = (this[APPLICATION_KEY] as Application).applicationContext
+                val db = Room.databaseBuilder(context, BdbDatabase::class.java, "bdb").build()
+                val accountRepo = AccountRepository(db.accounts())
+                SummaryScreenViewModel(accountRepo)
+            }
+        }
+    }
+}
